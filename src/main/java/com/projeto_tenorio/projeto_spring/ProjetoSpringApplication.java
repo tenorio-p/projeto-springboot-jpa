7d8e55bb7d842db5655673fd8f35d86a9c5471fd
Peterson Tenorio <petersontenorio10@gmail.com>
2026-03-12 14:05:34 -0300

    Projeto criado

---
+++ src/main/java/com/projeto_tenorio/projeto_spring/ProjetoSpringApplication.java
@@ -0,0 +1,13 @@
+package com.projeto_tenorio.projeto_spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetoSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjetoSpringApplication.class, args);
+	}
+
+}
