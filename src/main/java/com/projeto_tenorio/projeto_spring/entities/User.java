1c956b9523864177ca84db0ec4ec13e8487cd7a6
Peterson Tenorio <petersontenorio10@gmail.com>
2026-03-30 10:48:10 -0300

    User Delete

--- src/main/java/com/projeto_tenorio/projeto_spring/entities/User.java
+++ src/main/java/com/projeto_tenorio/projeto_spring/entities/User.java
@@ -25,7 +25,7 @@ public class User implements Serializable {
 
     @JsonIgnore
     @OneToMany(mappedBy = "client")
-    private List<Order> orders = new ArrayList<>();
+    private final List<Order> orders = new ArrayList<>();
 
     public User() {
     }
--- src/main/java/com/projeto_tenorio/projeto_spring/resources/UserResource.java
+++ src/main/java/com/projeto_tenorio/projeto_spring/resources/UserResource.java
@@ -40,4 +40,10 @@ public class UserResource {
         return ResponseEntity.created(uri).body(obj);
     }
 
+    @DeleteMapping(value = "/{id}")
+    public ResponseEntity<Void> deleteById(@PathVariable Long id){
+        userService.delete(id);
+        return ResponseEntity.noContent().build();
+    }
+
 }
