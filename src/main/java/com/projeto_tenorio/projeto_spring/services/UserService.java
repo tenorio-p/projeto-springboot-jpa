e0a4f456a496b2af9288e4b549b6496fd597bffa
Peterson Tenorio <petersontenorio10@gmail.com>
2026-03-30 13:51:42 -0300

    Exception database

--- src/main/java/com/projeto_tenorio/projeto_spring/services/UserService.java
+++ src/main/java/com/projeto_tenorio/projeto_spring/services/UserService.java
@@ -2,8 +2,11 @@ package com.projeto_tenorio.projeto_spring.services;
 
 import com.projeto_tenorio.projeto_spring.entities.User;
 import com.projeto_tenorio.projeto_spring.repository.UserRepository;
+import com.projeto_tenorio.projeto_spring.services.exceptions.DatabaseException;
 import com.projeto_tenorio.projeto_spring.services.exceptions.ResourceNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -29,7 +32,13 @@ public class UserService {
     }
 
     public void delete(Long id){
-        userRepository.deleteById(id);
+        try{
+            userRepository.deleteById(id);
+        }catch(EmptyResultDataAccessException e){
+            throw new ResourceNotFoundException(id);
+        }catch(DataIntegrityViolationException e){
+            throw new DatabaseException(e.getMessage());
+        }
     }
 
     public User update(Long id, User obj){
---
+++ src/main/java/com/projeto_tenorio/projeto_spring/services/exceptions/DatabaseException.java
@@ -0,0 +1,12 @@
+package com.projeto_tenorio.projeto_spring.services.exceptions;
+
+import java.io.Serial;
+
+public class DatabaseException extends RuntimeException {
+    @Serial
+    private static final long serialVersionUID = 1L;
+    public DatabaseException(String message) {
+        super(message);
+    }
+
+}
