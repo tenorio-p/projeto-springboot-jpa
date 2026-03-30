dced12a2089e9105bc7422e491e8fcc888c86e33
Peterson Tenorio <petersontenorio10@gmail.com>
2026-03-30 10:40:22 -0300

    Inserindo User Insert

--- src/main/java/com/projeto_tenorio/projeto_spring/resources/UserResource.java
+++ src/main/java/com/projeto_tenorio/projeto_spring/resources/UserResource.java
@@ -6,11 +6,10 @@ import com.projeto_tenorio.projeto_spring.services.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+import java.net.URI;
 import java.util.List;
 
 @RestController
@@ -27,11 +26,18 @@ public class UserResource {
 
     }
 
-
     @GetMapping(value = "/{id}")
     public ResponseEntity<User> findById(@PathVariable Long id) {
         User obj =  userService.findById(id);
         return ResponseEntity.ok().body(obj);
+    }
+
+    @PostMapping
+    public ResponseEntity<User> insert(@RequestBody User obj){
+        obj = userService.insert(obj);
+        URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}")
+                .buildAndExpand(obj.getId()).toUri();
+        return ResponseEntity.created(uri).body(obj);
     }
 
 }
